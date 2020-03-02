b17ccedec35eb287cbf9c01fdf2e4d30b678e729
amrvitale <54391413+amrvitale@users.noreply.github.com>
2020-03-02 14:03:22 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -6,14 +6,24 @@ import Counter from './state/Counter';
 import HelloWorld from './state-drills/HelloWorld';
 import Bomb from './state-drills/Bomb';
 import RouletteGun from './state-drills/RouletteGun';
+import Tabs from './state/Tabs';
 
-class App extends Component {
+
+  const tabsProp = [
+    { name: 'First tab',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.' },
+    { name: 'Second tab',
+      content: 'Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
+    { name: 'Third tab',
+      content: 'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.' },
+  ];
+
+  class App extends Component {
   render() {
     return (
       <div className="App">
-      <RouletteGun />
 
-        <br /> <br /> <br />
+        <Tabs tabs={tabsProp}/>
       </div>
     );
   }
@@ -30,5 +40,6 @@ export default App;
   <br /><br /><br />
   <Counter count ={123} />
   <Bomb />
+  <RouletteGun />
 
     <br /><br /><br /> */
